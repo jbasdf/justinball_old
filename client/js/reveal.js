6c2b4ece838d12982f6ff6b285be29a2a7598810
Justin Ball <justinball@gmail.com>
2016-04-02 22:26:44 -0600

    Adds reveal

---
+++ client/js/reveal.js
@@ -0,0 +1,19 @@
+import Reveal  from 'reveal.js';
+
+var type = window.location.search.match( /print-pdf/gi );
+require(`reveal.js/css/print/${type}.css`);
+
+// Full list of configuration options available here:
+// https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+
+  transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+
+  // Parallax scrolling
+  // parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+  // parallaxBackgroundSize: '2100px 900px',
+});
