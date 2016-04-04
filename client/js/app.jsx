65bc686cef5f3715eaa571fbc8d9b2eb66601c50
Justin Ball <justinball@gmail.com>
2016-04-03 18:38:47 -0600

    Don't require styles in app.jsx

--- client/js/app.jsx
+++ client/js/app.jsx
@@ -17,8 +17,6 @@ import jwt                    from './loaders/jwt';
 import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
 
-require("../styles/styles.less");
-
 class Root extends React.Component {
   render(){
     const devTools = __DEV__ ? <DevTools /> : null;
