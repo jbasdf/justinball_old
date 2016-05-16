47e45d10a351db324f0a30a02d20786a87f14057
Justin Ball <justinball@gmail.com>
2016-05-15 19:31:05 -0600

    Working on horizontal menu

--- client/themes/stripy/js/application.js
+++ client/themes/stripy/js/application.js
@@ -1,7 +1,37 @@
-var toggle = document.getElementById('toggle');
-if(toggle){
-  document.getElementById('toggle').addEventListener('click', function(e){
-    document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+(function(window, document) {
+  var menu = document.getElementById('menu'),
+    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
+
+  function toggleHorizontal() {
+    [].forEach.call(
+      document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+      function(el) {
+        el.classList.toggle('pure-menu-horizontal');
+      }
+    );
+  };
+
+  function toggleMenu() {
+    // set timeout so that the panel has a chance to roll up
+    // before the menu switches states
+    if(menu.classList.contains('open')) {
+      setTimeout(toggleHorizontal, 500);
+    } else {
+      toggleHorizontal();
+    }
+    menu.classList.toggle('open');
     document.getElementById('toggle').classList.toggle('x');
+  };
+
+  function closeMenu() {
+    if(menu.classList.contains('open')) {
+      toggleMenu();
+    }
+  }
+
+  document.getElementById('toggle').addEventListener('click', function(e) {
+    toggleMenu();
   });
-}
+
+  window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+})(window, window.document);
