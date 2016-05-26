3fb7ccd583b860c53ae6613efa62f1dc6e8f90ad
Justin Ball <justinball@gmail.com>
2016-05-25 22:17:46 -0600

    Don't crash if html minify fails

--- client/tools/build/production.js
+++ client/tools/build/production.js
@@ -4,12 +4,16 @@ var webpackUtils  = require("./webpack_utils");
 module.exports = function(html, stage, webpackConfig, webpackStats, options){
   if(stage == "production"){
     html = webpackUtils.apply(html, webpackConfig, webpackStats, options.entries, options.cssEntries, options.buildSuffix);
-    return minify(html, {
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyJS: true
-    });
-  } else {
-    return html;
+    try{
+      return minify(html, {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true
+      });
+    } catch(err){
+      console.log("Error while minifying html: " + err);
+      debugger;
+    }
   }
+  return html;
 };
