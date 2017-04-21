ad7afa571a8d0c8f616d0d26c4a74f4c1e34c867
Justin Ball <justinball@gmail.com>
2017-04-21 13:55:05 -0600

    Always include app name in publicPath

--- client/config/settings.js
+++ client/config/settings.js
@@ -104,7 +104,7 @@ function outputPaths(name, port, options) {
   // Public path indicates where the assets will be served from. In dev this will likely be
   // localhost or a local domain. In production this could be a CDN. In developerment this will
   // point to whatever public url is serving dev assets.
-  const urlPath = options.hotPack && !_.isEmpty(name) ? `/${name}` : '';
+  const urlPath = !_.isEmpty(name) ? `/${name}` : '';
   let publicPath = `${devAssetsUrl}${urlPath}${devRelativeOutput}`;
 
   if (isProduction(options.stage)) {
